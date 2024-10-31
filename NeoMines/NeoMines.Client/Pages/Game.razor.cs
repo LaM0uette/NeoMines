2eb57fe95af099e3635b1de2791e066bd9b86f27
LaM0uette <LaM0uette.dev@gmail.com>
2024-10-31 02:44:59 +0100

    create basic Home page and Game page

---
+++ NeoMines/NeoMines.Client/Pages/Game.razor.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NeoMines.Client.Pages;
+
+public class GameBase : ComponentBase
+{
+}
---
+++ NeoMines/NeoMines.Client/Pages/Home.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NeoMines.Client.Pages;
+
+public class HomeBase : ComponentBase
+{
+    #region Statements
+
+    [Inject] private NavigationManager _navigationManager { get; set; } = null!;
+
+    #endregion
+
+    #region HomeBase
+
+    protected void GoToGamePage()
+    {
+        _navigationManager.NavigateTo("/Game");
+    }
+
+    #endregion
+}
